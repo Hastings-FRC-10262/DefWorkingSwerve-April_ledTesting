83fc402630727d2d0c14e5ea937f2dbe2f8926f2
SnapRap <109704838+IanBMorrison@users.noreply.github.com>
2026-01-11 00:30:37 -0500

    SmallUpdate

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -136,7 +136,7 @@ public class RobotContainer
 
     if (RobotBase.isSimulation())
     {
-      drivebase.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
+      drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
     } else
     {
       drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
