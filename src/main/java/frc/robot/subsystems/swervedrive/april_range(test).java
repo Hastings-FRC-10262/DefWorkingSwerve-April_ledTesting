ce88d551d1a2d36f72f9c855e8f82b629b464b57
KabirSindhwani <sindhwani.kabir@gmail.com>
2026-01-13 16:24:35 -0500

    Fix variable name case for limelightName

--- src/main/java/frc/robot/subsystems/swervedrive/april_range(test).java
+++ src/main/java/frc/robot/subsystems/swervedrive/april_range(test).java
@@ -32,7 +32,7 @@ public class Limelight_LED_Test extends SubsystemBase {
 
         if (seesAprilTag) {
                         
-            double angle = LimelightHelpers.getTX(LimelightName);
+            double angle = LimelightHelpers.getTX(limelightName);
             
             var tag = results.targets_Fiducials[0];
 
